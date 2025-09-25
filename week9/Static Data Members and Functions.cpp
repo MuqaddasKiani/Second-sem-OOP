3d599c0226728aee4cae4d062ddb1e885177a26a
MuqaddasKiani <kianimuqaddass@gmail.com>
2025-09-25 20:16:19 +0500

    Static Data Members and Functions

---
+++ week9/Static Data Members and Functions.cpp
@@ -0,0 +1,52 @@
+//Static Data Members and Functions
+                              
+//                              TASK1
+
+//#include <iostream>
+//using namespace std;
+//
+//class MyClass {
+//    static int count;
+//public:
+//    MyClass() {
+//        count++;
+//    }
+//    static void showCount() {
+//        cout << "Number of objects created: " << count << endl;
+//    }
+//};
+//
+//int MyClass::count = 0;
+//
+//int main() {
+//    MyClass a, b, c;
+//    MyClass::showCount();
+//    return 0;
+//}
+
+//                               TASK2
+
+//#include <iostream>
+//using namespace std;
+//
+//class MyClass {
+//    static int count;
+//public:
+//    MyClass() {
+//        count++;
+//    }
+//    static void print() {
+//        cout << "Total objects so far: " << count << endl;
+//    }
+//};
+//
+//int MyClass::count = 0;
+//
+//int main() {
+//    MyClass obj1, obj2;
+//    MyClass::print();
+//    MyClass obj3;
+//    MyClass::print();
+//    return 0;
+//}
+
